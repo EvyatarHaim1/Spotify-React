29824281e44d352675254051e3702cb27de28193
Evyatar Haim <evyataros1@gmail.com>
2020-11-11 16:38:51 +0200

    using Spotify API getMe function to fetch my real spotify user data

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, {useState,useEffect} from 'react';
 import './App.css';
 import Login from './components/Login';
 import { getTokenFromUrl } from './spotify';
+import SpotifyWebApi from 'spotify-web-api-js';
+
+const spotify = new SpotifyWebApi();
 
 function App() {
 
@@ -15,6 +18,12 @@ function App() {
 
     if (_token) {
       setToken(_token)
+
+      spotify.setAccessToken(_token);
+      
+      spotify.getMe().then(user => {
+        console.log('🤠',user);
+      })
     }
 
   }, [])
@@ -24,7 +33,7 @@ function App() {
     <div className="App">
       {
         token ? (
-          <h1>I'm the Player</h1> 
+          <h1>I'm logged in</h1> 
         ) : (<Login /> )
       }
     </div>
