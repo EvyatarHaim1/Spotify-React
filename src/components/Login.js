6b25f9a60cba2cc2a7ac0042ba568dc3d9a07c00
Evyatar Haim <evyataros1@gmail.com>
2020-11-11 14:26:06 +0200

    connecting the Login component to the real Spotify Login page by clicking the button and redirect bak to the local app with the user data which authenticated

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import styled from 'styled-components';
+import {loginUrl} from '../spotify';
 //  const animation = "https://i.pinimg.com/originals/27/6f/27/276f273d11f8b9dbc0a9c55bb38ea8c6.gif";
+
 function Login() {
     return (
         <LOGIN>
           <img 
             src="https://services.tegrazone.com/sites/default/files/pcgame-featured-image/Spotify-MFG-840x360.jpg" 
             alt="spotify-logo" />
-          <a>LOGIN WITH SPOTIFY</a>
+          <a href={loginUrl}>LOGIN WITH SPOTIFY</a>
         </LOGIN>
     )
 }
---
+++ src/spotify.js
@@ -0,0 +1,18 @@
+
+export const authEndpoint = 
+"https://accounts.spotify.com/authorize";
+
+const redirectUri = "http://localhost:3000/";
+
+const clientId = "674cb6d064aa4d8eaffe5aa706a12bd6";
+
+const scopes = [
+    "user-read-currently-playing",
+    "user-read-recently-played",
+    "user-read-playback-state",
+    "user-top-read",
+    "user-modify-playback-state",
+];
+
+export const loginUrl =
+`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`;
